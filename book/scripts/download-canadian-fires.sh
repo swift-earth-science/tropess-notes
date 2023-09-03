899a1ce4f055c33880e0c12261248676351c52e7
vkantchev <vkantchev@users.noreply.github.com>
2023-09-02 21:14:03 -0700

    Use `gdate` instead of `date` command on macOS

--- book/scripts/download-canadian-fires.sh
+++ book/scripts/download-canadian-fires.sh
@@ -5,8 +5,8 @@ start_date=2023-06-01
 end_date=2023-06-09
 
 # after this, start_date and end_date will be valid ISO 8601 dates,
-start_date=$(date -I -d "$start_date") || exit 1
-end_date=$(date -I -d "$end_date") || exit 1
+start_date=$(gdate -I -d "$start_date") || exit 1
+end_date=$(gdate -I -d "$end_date") || exit 1
 
 pushd ./data
 
@@ -14,15 +14,15 @@ current_date="$start_date"
 while [ "$current_date" != "$end_date" ]; do 
     echo Downloading $current_date
 
-    file_date=$(date -d "$current_date" +"%Y%m%d")
+    file_date=$(gdate -d "$current_date" +"%Y%m%d")
     data_file="TROPESS_CrIS-JPSS1_L2_Summary_CO_${file_date}_MUSES_R1p20_FS_F0p6.nc"
 
     wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies \
-         --auth-no-challenge=on --keep-session-cookies --content-disposition \
+         --auth-no-challenge=on --keep-session-cookies --content-disposition --continue \
         https://tropess.gesdisc.eosdis.nasa.gov/data/TROPESS_Summary/TRPSYL2COCRS1FS.1/2023/$data_file
 
     # advance to next date
-    current_date=$(date -I -d "$current_date + 1 day")
+    current_date=$(gdate -I -d "$current_date + 1 day")
 done
 
 popd

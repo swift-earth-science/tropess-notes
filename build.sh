4bddb4186fa315d4d9d4d9d7f36cb4f824ecd96f
vkantchev <vkantchev@users.noreply.github.com>
2024-06-09 10:01:46 -0700

    Fix a bug that generated wrong canonical urls

--- build.sh
+++ build.sh
@@ -4,5 +4,13 @@ source activate.sh
 
 jupyter-book build --builder dirhtml --all book/
 
+# fix https://github.com/executablebooks/jupyter-book/issues/2066
+find book/_build/dirhtml \
+    -type f \
+    -name "index.html" \
+    -exec sed -i '' 's|<link rel="canonical" href="https://tropess-notes.swiftearthscience.org/\(.*\)\.html" />|<link rel="canonical" href="https://tropess-notes.swiftearthscience.org/\1/" />|g' {} \;
+
+sed -i '' 's|<link rel="canonical" href="https://tropess-notes.swiftearthscience.org/index/" />|<link rel="canonical" href="https://tropess-notes.swiftearthscience.org/" />|g' book/_build/dirhtml/index.html
+
 cp book/CNAME book/_build/dirhtml
 touch book/_build/dirhtml/.nojekyll

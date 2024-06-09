40fcee82cad643319c85def91a7dc84064029727
vkantchev <vkantchev@users.noreply.github.com>
2024-06-09 10:12:16 -0700

    Fix a bug that generated wrong canonical urls

--- build.sh
+++ build.sh
@@ -12,5 +12,6 @@ find book/_build/dirhtml \
 
 sed -i '' 's|<link rel="canonical" href="https://tropess-notes.swiftearthscience.org/index/" />|<link rel="canonical" href="https://tropess-notes.swiftearthscience.org/" />|g' book/_build/dirhtml/index.html
 
+# copy CNAME file to the build directory
 cp book/CNAME book/_build/dirhtml
 touch book/_build/dirhtml/.nojekyll
